c51dafc302d029853c62b652d2ca5fe62a602d78
Jake Parks <jakeparks5000@gmail.com>
2019-01-26 18:23:19 -0600

    add front-end react code

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.css'
 import App from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
